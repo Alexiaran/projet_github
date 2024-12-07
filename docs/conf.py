1f5d13b96866cc2bdfe4b6c8ab2d603936ecda29
Randazavony <alexiarandazavony@gmail.com>
2024-12-07 22:13:12 +0100

    Reformat docs/conf.py with black

--- docs/conf.py
+++ docs/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'projet git hub'
-copyright = '2024, Randazavony'
-author = 'Randazavony Alexia'
-release = '0.1'
+project = "projet git hub"
+copyright = "2024, Randazavony"
+author = "Randazavony Alexia"
+release = "0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-#extensions = []
+# extensions = []
 
-#templates_path = ['_templates']
-#exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+# templates_path = ['_templates']
+# exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'alabaster'
-#html_static_path = ['_static']
+# html_theme = 'alabaster'
+# html_static_path = ['_static']
