e4c9111a42519783dd3e56b96d49ff5555d8b904
Randazavony <alexiarandazavony@gmail.com>
2024-12-10 22:26:31 +0100

    amelioration du coverage : fonction retourner livre et affficher cataloge

--- tests/test_main.py
+++ tests/test_main.py
@@ -35,11 +35,17 @@ def test_emprunter_livre(bibliotheque):
 
 
 def test_retourner_livre(bibliotheque):
+    # Cas où le livre a été emprunté et est retourné
     bibliotheque.emprunter_livre("1984")
     assert bibliotheque.retourner_livre("1984") is True
     livre = next(a for a in bibliotheque.catalogue if a.titre == "1984")
     assert livre.emprunte is False
+
+    # Cas où le livre est déjà retourné
     assert bibliotheque.retourner_livre("1984") is False  # Déjà retourné
+
+    # Cas où le livre n'a jamais été emprunté
+    assert bibliotheque.retourner_livre("Livre Inexistant") is False
 
 
 def test_rechercher_livre(bibliotheque):
@@ -52,3 +58,4 @@ def test_afficher_catalogue(bibliotheque):
     catalogue = bibliotheque.afficher_catalogue()
     assert len(catalogue) == 3
     assert "1984 par George Orwell (1949) [Disponible]" in catalogue
+    assert "Le Meilleur des Mondes par Aldous Huxley (1932) [Disponible]" in catalogue
